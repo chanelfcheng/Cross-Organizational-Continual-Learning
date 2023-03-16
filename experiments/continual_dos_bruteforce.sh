d36c35f4a59a82fb4d38027a089e5eb978cb2d9a
Chanel Cheng <chanelchengc@gmail.com>
2023-03-15 22:27:22 -0400

    Added new experiments and updated VAE

--- experiments/continual_dos_bruteforce.sh
+++ experiments/continual_dos_bruteforce.sh
@@ -3,6 +3,8 @@
 
 cd ..
 
+# NOTE: this experiment does not run currently due to not being able to one-hot
+# encode the protocols (missing benign class).
 # TODO: change --dataset-paths to the correct path where your data is stored
 python continual.py \
 --exp-name $(basename ${0%".sh"}) \
